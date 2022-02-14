c6a22116d633b23fa77b894ece34c9f3fd50d39e
FrancescoGM <francescogmuller@hotmail.com>
2022-02-14 02:02:37 -0300

    feat(configuration): jest was added to do test suits

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import { InitialOptionsTsJest, pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.json'
+
+export default {
+  bail: true,
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).(ts|js)'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+} as InitialOptionsTsJest
